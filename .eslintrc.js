82f15d09f0c26b587849d74e3738be7269b556b0
Simon Scarfe <simon.scarfe@bbc.co.uk>
2018-03-29 23:55:52 +0100

    this isn't parallelisable currently. Some Puppeteer voodoo needed

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,19 @@ module.exports = {
         trailingComma: 'all',
       },
     ],
+    'no-await-in-loop': 0,
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
 };
