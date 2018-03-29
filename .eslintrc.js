ea582305dd0df524c35af4097e73a70db94a114a
Simon Scarfe <simon.scarfe@bbc.co.uk>
2018-03-29 18:00:47 +0100

    bare basics

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+          singleQuote: true, 
+          trailingComma: 'all',
+      },
+    ]
+  },
+};
